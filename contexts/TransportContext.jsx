0182333f2542242fc23c82932326ea11cd7191a7
Eevee Kreevee <thekrambuhl@gmail.com>
2020-11-04 12:40:09 -0800

    add transport context

---
+++ contexts/TransportContext.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const TransportContext = React.createContext({
+  startTime: 0,
+  time: 0,
+  setTime: () => {}
+})
+
+export function TransportProvider({ children }) {
+  const timer = useRef(null)
+  const [startTime, setStartTime] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    setStartTime(Date.now())
+  }, [])
+
+  useEffect(() => {
+    setCurrentTime(Date.now())
+
+    timer.current = setInterval(() => {
+      setCurrentTime(Date.now())
+    }, 1000 / 60)
+
+    return () => clearInterval(timer.current)
+  }, [])
+
+  return (
+    <TransportContext.Provider value={{ startTime, currentTime, time, setTime }}>
+      {children}
+    </TransportContext.Provider>
+  )
+}
+
+export default TransportContext
---
+++ contexts/index.js
@@ -0,0 +1,4 @@
+export {
+  default as TransportContext,
+  TransportProvider
+} from './TransportContext'
