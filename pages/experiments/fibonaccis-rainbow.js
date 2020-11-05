5d468c25bcbfa3f33127c274c5c7f98ae0b4a7e2
Eevee Kreevee <thekrambuhl@gmail.com>
2020-11-04 19:25:10 -0800

    paint colors

--- pages/experiments/fibonaccis-rainbow.js
+++ pages/experiments/fibonaccis-rainbow.js
@@ -1,23 +1,44 @@
 import { Transport, VisualContainer } from '@components'
 import { useTransportContext, TransportProvider } from '@contexts'
 
-const rows = Array.from({ length: 15 }).map((_, x) => x)
-const columns = Array.from({ length: 28 }).map((_, x) => x)
+function fibonacci(num) {
+  const list = [0, 1]
 
-const steps = [1, 1, 2, 3, 5, 8, 13, 21]
-const stepsRev = [13, 8, 5, 3, 2, 1, 1]
+  while (list.length < num) {
+    const a = list[list.length - 1]
+    const b = list[list.length - 2]
+    list.push(a + b)
+  }
 
-const colors = [
-  '#fd5',
-  '#fb4',
-  '#f92',
-  '#f72',
-  '#f51',
-  '#f11',
-  '#b04',
-  '#09f',
-  '#0ee',
-]
+  return list
+}
+
+const rows = Array.from({ length: 6 }).map((_, x) => x)
+const columns = Array.from({ length: 50 }).map((_, x) => x)
+
+const steps = fibonacci(12)
+const stepsRev = fibonacci(12)
+
+const paintColorsMap = {
+  Viola: '#A067C3',
+  Lavender: '#5A4D9A',
+  BlueVelvet: '#361E67',
+  // PurpleRed: '#4E1923',
+  // RoyalRed: '#7A1A1E',
+  // Brick: '#98221E',
+  BloodOrange: '#CD361F',
+  // FireRed: '#FC5648',
+  Orangina: '#F07C31',
+  YellowCab: '#E9AC38',
+  LawnGreen: '#5B9E47',
+  Malachite: '#58BABB',
+  // MalachiteDark: '#1D817F',
+  SkyBlue: '#1079CB',
+  // Fjord: '#244363',
+  // DeepSea: '#294056',
+}
+
+const paintColors = Object.values(paintColorsMap)
 
 function FibonacciRainbow () {
   const { frame } = useTransportContext()
@@ -33,15 +54,15 @@ function FibonacciRainbow () {
               <div key={x} className="row">
                 {
                   columns.map(y => {
-                    const x1 = x + 3
-                    const y1 = y + 2
+                    const x1 = x + 0
+                    const y1 = y + 0
                     const slowFrame = (frame * 0.5)
 
                     const res = (
-                      (x * 120) +
-                      (y * 8) +
-                      steps[Math.floor(x1 / 1) % steps.length] +
-                      stepsRev[Math.floor(y1 / 1) % stepsRev.length] +
+                      // (x * 120) +
+                      // (y * 8) +
+                      // steps[Math.floor(x1 / 1) % paintColors.length] +
+                      steps[Math.floor(y1 / 1) % paintColors.length] +
                       slowFrame
                     )
 
@@ -50,7 +71,7 @@ function FibonacciRainbow () {
                         key={y}
                         className="cell"
                         style={{
-                          backgroundColor: colors[Math.floor(res % colors.length)]
+                          backgroundColor: paintColors[Math.floor(res % paintColors.length)]
                         }}
                       />
                     )
@@ -68,32 +89,38 @@ function FibonacciRainbow () {
           flex-flow: column wrap;
           align-content: center;
           padding: var(--space-md);
+          width: 100%;
         }
 
         .row {
           display: flex;
+          margin-top: 2px;
+          justify-content: center;
+          width: 100%;
         }
 
         .cell {
-          width: clamp(16px, 2vw, 32px);
-          height: clamp(16px, 2vw, 32px);
+          width: clamp(16px, 2vw, 20px);
+          height: clamp(16px, 10vh, 72px);
           background-color: white;
           color: black;
         }
 
         @media (max-width: 720px) {
           .root {
-            max-width: 360px;
             flex-flow: row-reverse wrap;
           }
 
           .row {
             flex-flow: column wrap;
+            margin-top: 0;
+            margin-left: 2px;
+            width: auto
           }
 
           .cell {
-            height: max(12px, 3vw);
-            width: max(12px, 3vw);
+            width: clamp(32px, 10vw, 60px);
+            height: clamp(10px, 1.5vh, 14px);
           }
         }
       `}</style>
