83883c2981a75ab20380d6641951d66919709625
Eevee Kreevee <thekrambuhl@gmail.com>
2020-11-26 22:49:16 -0800

    2

--- pages/experiments/index.js
+++ pages/experiments/index.js
@@ -80,6 +80,7 @@ export async function getStaticProps(context) {
     { title: 'sequencing machine', url: 'sequencing-machine', date: '2020-11-24' },
     { title: 'spiral galaxy tv', url: 'spiral-galaxy-tv', date: '2020-11-24' },
     { title: 'turkey tumble', url: 'turkey-tumble', date: '2020-11-26' },
+    { title: 'turkey tumble 2', url: 'turkey-tumble-2', date: '2020-11-26' },
   ]
 
   const projects = reverse(sortBy(
