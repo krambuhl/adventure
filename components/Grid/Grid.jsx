c6a8bcea8a39791d894c4905e23638a78b29d54c
Eevee Kreevee <thekrambuhl@gmail.com>
2020-12-03 13:05:11 -0800

    capture grid

--- components/Grid/Grid.jsx
+++ components/Grid/Grid.jsx
@@ -10,71 +10,45 @@ export default function Grid ({
   className,
   ...props
 }) {
-  const classList = classnames('root', className)
+  const classList = classnames(css.root, className)
   const rows = Array.from({ length: rowsLength }).map((_, x) => x)
   const columns = Array.from({ length: columnsLength }).map((_, x) => x)
 
   return (
     <>
       <div className={classList} {...props}>
-        {
-          rows.map(y => {
-            return (
-              <div key={y} className="row">
-                {
-                  columns.map(x => {
-                    const style = getCellStyle({ x, y })
+        <div className={css.grid}>
+          {
+            rows.map(y => {
+              return (
+                <div key={y} className={css.row}>
+                  {
+                    columns.map(x => {
+                      const style = getCellStyle({ x, y })
 
-                    return (
-                      <div
-                        key={x}
-                        className={classnames(css.cell, 'cell')}
-                        style={style}
-                      />
-                    )
-                  })
-                }
-              </div>
-            )
-          })
-        }
+                      return (
+                        <div
+                          key={x}
+                          className={classnames(css.cell, 'cell')}
+                          style={style}
+                        />
+                      )
+                    })
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
 
       <style jsx>{`
-        .root {
-          display: flex;
-          flex-flow: column wrap;
-          align-content: center;
-          padding: var(--space-md);
-          width: 100%;
-        }
-
-        .row {
-          display: flex;
-          margin-top: 0px;
-          justify-content: center;
-          width: 100%;
-        }
-
         .cell {
           width: ${cellSize[0]};
           height: ${cellSize[1]};
-          background-color: transparent;
-          color: black;
         }
 
         @media (max-width: 720px) {
-          .root {
-            flex-flow: row-reverse wrap;
-          }
-
-          .row {
-            flex-flow: column-reverse wrap;
-            margin-top: 0;
-            margin-left: 0px;
-            width: auto
-          }
-
           .cell {
             width: ${mobileCellSize[0]};
             height: ${mobileCellSize[1]};
