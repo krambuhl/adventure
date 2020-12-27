a372f3104dbeaf7f5b35750e2dbd8a4abad020c7
Eevee Kreevee <thekrambuhl@gmail.com>
2020-12-26 17:56:49 -0800

    checckout 1240-1260

---
+++ pages/experiments/2020/12/big-moves.js
@@ -0,0 +1,69 @@
+import { useEffect } from 'react'
+import { Grid, Transport, VisualContainer } from '@components'
+import { useTransportContext, withTransportProvider } from '@contexts'
+import { outdoorPaint as color } from '@data/colorMaps'
+
+export default withTransportProvider(Output)
+export const meta = {
+  date: '2020-12-26'
+}
+
+const size = 36 / (15 / 32)
+
+const colors = [
+  color.Lavender,
+  color.Viola,
+  color.BloodOrange,
+  color.Orangina,
+  color.YellowCab,
+  color.LawnGreen,
+  color.Malachite,
+  color.SkyBlue,
+]
+
+function Output () {
+  const { frame, setFrame } = useTransportContext()
+
+  useEffect(() => {
+    setFrame(1250)
+  }, [])
+
+  return (
+    <VisualContainer>
+      <Transport />
+
+      <Grid
+        rows={size}
+        columns={size}
+        cellSize={[`${512 / size}px`, `${512 / size}px`]}
+        mobileCellSize={[`${90 / size}vw`, `${135 / size}vw`]}
+        getCellStyle={({ x, y }) => {
+          const x1 = x + 1
+          const y1 = y + 1
+          const x0 = x1 - (size / 2)
+          const y0 = y1 - (size / 2)
+
+          const slowFrame = frame * 0.2
+          const slowerFrame = frame * 0.015
+          const slowestFrame = frame * 0.001
+
+          const res = (
+            (colors.length - 1) *
+            (
+              Math.sin(y0 * slowerFrame) * 4.14 + Math.sin(x0 * slowestFrame) * 4.3
+            ) +
+            0
+          )
+
+          if (isNaN(res)) return {
+            backgroundColor: colors[0]
+          }
+
+          return {
+            backgroundColor: colors[Math.floor(Math.abs(res) % colors.length)]
+          }
+        }}
+      />
+    </VisualContainer>
+  )
+}
